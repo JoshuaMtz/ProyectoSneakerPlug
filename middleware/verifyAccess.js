1524962c7d6c364a6bc920ff3b7f2eb2562dd58a
JoshuaMtz <a01703723@itesm.mx>
2021-07-28 10:34:04 -0500

    Pagina y middleware para vender productos

---
+++ middleware/verifyAccess.js
@@ -0,0 +1,16 @@
+function verifyToken(req,res,next) {
+
+    var token = req.cookies.token || '' ; 
+    
+    if (!token) {
+    
+        return res.redirect('/login')
+    }
+    else {
+    
+        next();
+    }
+    
+}    
+    
+module.exports = verifyToken;
